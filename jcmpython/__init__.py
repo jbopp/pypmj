6112f3df94e89609976b905ac2968894314866ed
Carlo Barth <carlo.barth@helmholtz-berlin.de>
2016-09-13 14:16:00 +0200

    Changed version to 2.2.0.
    This version of jcmpython contains some larger changes. Simulations can
    now be solved without being part of a simulation set. Resource handling
    is now unified using the `ResourceManager`-class, which is used
    throughout the `core` module.
    
    The new extension `antenna` is now available, thanks to Niko. He was
    also added to the contributors file.

--- jcmpython/__init__.py
+++ jcmpython/__init__.py
@@ -25,7 +25,7 @@ Copyright(C) 2016 Carlo Barth.
 __author__ = 'Carlo Barth'
 __copyright__ = 'Copyright 2016'
 __license__ = 'GPL'
-__version__ = '2.1.4'
+__version__ = '2.2.0'
 __maintainer__ = 'Carlo Barth'
 __status__ = 'Production'
 
